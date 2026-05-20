dbd4b352fe1d9227822064de368c7e4c36e2cd1c
Vupiad <12345vupham@gmail.com>
2026-05-20 17:30:08 +0700

    fix the pdf display bug

--- frontend/src/pdf.ts
+++ frontend/src/pdf.ts
@@ -1,4 +1,6 @@
 import { Document, Page, pdfjs } from 'react-pdf';
+import 'react-pdf/dist/Page/AnnotationLayer.css';
+import 'react-pdf/dist/Page/TextLayer.css';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
